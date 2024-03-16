c8fcd9d19044b0b7c410952827cd24902707fbd4
mishra161989 <57952912+mishra161989@users.noreply.github.com>
2024-03-16 13:57:17 +0530

    Added components for airbnb data stream ingestion for cicd

--- lambda_function_s3_write.py
+++ lambda_function_s3_write.py
@@ -1,23 +1,20 @@
 import json
 import boto3
+from datetime import datetime
 
 
 def lambda_handler(event, context):
     event_data = json.dumps(event)
-
-    # Define the S3 bucket and key where you want to store the data
-    # bucket_name = 'your-bucket-name'
-    # key = 'event_data.json'  # Example key name
     print(event_data)
     # Create an S3 client
     s3_client = boto3.client('s3')
-
-    # Write the event data to the S3 bucket
+    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    s3_key = f"hk-airbnb-booking-records-{timestamp}.json"
     try:
         response = s3_client.put_object(
             Body=event_data,
             Bucket='hk-airbnb-booking-records',
-            Key='hk-airbnb-booking-records.csv'
+            Key=s3_key
         )
         print("Event data has been written to S3 successfully.")
         return {
